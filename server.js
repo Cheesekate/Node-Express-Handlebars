4814a878852272879051bc500ca9c1aa87992b0a
Kathryn wade <kathrynwade@Kathryns-MacBook.local>
2020-06-12 21:36:50 -0700

    server not saved on last push

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-var routes = require("./controllers/burgers.js");
+var routes = require("./controllers/burgers_controllers.js");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
