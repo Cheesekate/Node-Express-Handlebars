67585109633b65c18f833b23eb1aa7dbd25fafe9
Kathryn wade <katewade0@gmail.com>
2020-07-17 14:58:18 -0700

    final commit

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db",
-  insecureAuth: "true",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else {
+  mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
+    insecureAuth: "true",
+  });
+
+}
+
 
 
 
